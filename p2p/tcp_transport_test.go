5372ee85a943b40ad38bff8821dd3f7dee5c65b9
Dhruv Makwana <dhruv.makwana.169@gmail.com>
2024-08-02 18:13:04 -0500

    add TCP transport implementation with tests and update Makefile for command execution

---
+++ p2p/tcp_transport_test.go
@@ -0,0 +1,14 @@
+package p2p
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTCPTransport(t *testing.T) {
+	listenAddr := ":5000"
+	tr := NewTCPTransport(listenAddr)
+
+	assert.Equal(t, listenAddr, tr.listenAddress)
+}
---
+++ p2p/transport.go
@@ -0,0 +1,11 @@
+package p2p
+
+// Peer is the interface that represents the remote node in the network
+type Peer interface {
+}
+
+// Transport is the interface that handles the communication
+// between the nodes in the network. This can be of the
+// form (TCP, UDP, Websockets, etc)
+type Transport interface {
+}
