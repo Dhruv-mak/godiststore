eaf9325f3003a6374850b70aa51a651d2cf62a84
Dhruv Makwana <dhruv.makwana.169@gmail.com>
2024-08-13 16:16:17 -0500

    implement file storage and broadcasting in FileServer; enhance message handling and improve error logging

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"log"
+	"time"
 
 	"github.com/Dhruv-mak/godiststore/p2p"
 )
@@ -34,7 +36,13 @@ func main() {
 		log.Fatal(s1.Start())
 	}()
 
-	s2.Start()
+	time.Sleep(2 * time.Second)
+
+	go s2.Start()
+	time.Sleep(2 * time.Second)
+
+	data := bytes.NewReader([]byte("my big data file here!"))
+	s2.StoreData("myprivatedata", data)
 
 	select {}
 }
--- server.go
+++ server.go
@@ -52,40 +52,44 @@ func (s *FileServer) loop() {
 		case rpc := <-s.Transport.Consume():
 			var msg Message
 			if err := gob.NewDecoder(bytes.NewReader(rpc.Payload)).Decode((&msg)); err != nil {
-				log.Fatal(err)
+				log.Println(err)
+				return
+			}
+			if err := s.handleMessage(rpc.From, &msg); err != nil {
+				log.Println(err)
+				return
 			}
 
-			fmt.Printf("recv: %s\n", string(msg.Payload([]byte)))
-
-			peer, ok := s.peers[rpc.From.String()]
-			if !ok {
-				panic("peer not found in peer map")
-			}
-
-			b := make([]byte, 1000)
-			if _, err := peer.Read(b); err != nil {
-				panic(err)
-			}
-			fmt.Printf("recv: %s\n", string(b))
-
-			peer(*p2p.TCPPeer).Wg.Done()
-
-			// if err := s.handleMessage(&m); err != nil {
-			// 	log.Println(err)
-			// }
 		case <-s.quitch:
 			return
 		}
 	}
 }
 
-// func (s *FileServer) handleMessage(msg *Message) error {
-// 	switch v := msg.Payload.(type) {
-// 	case *DataMessage:
-// 		fmt.Printf("received data message: %s\n", v)
-// 	}
-// 	return nil
-// }
+func (s *FileServer) handleMessage(from string, msg *Message) error {
+	switch v := msg.Payload.(type) {
+	case *MessageStoreFile:
+		return s.handleMessageStorefile(from, v)
+	}
+	return nil
+}
+
+func (s *FileServer) handleMessageStorefile(from string, msg MessagStoreFile) error {
+	peer, ok := s.peers[from]
+	if !ok {
+		fmt.Errorf("peer (%s) not found in peer map", from)
+	}
+
+	n, err := s.store.Write(msg.key, io.LimitReader(peer, msg.Size))
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("written %d bytes to disk\n", n)
+	peer.(*p2p.TCPPeer).Wg.Done()
+
+	return nil
+}
 
 type Payload struct {
 	Key  string
@@ -96,13 +100,18 @@ type Message struct {
 	Payload any
 }
 
+type MessageStoreFile struct {
+	Key  string
+	Size int64
+}
+
 func (s *FileServer) broadcast(msg *Message) error {
 	peers := []p2p.Peer{}
 	for _, peer := range s.peers {
 		peers = append(peers, peer)
 	}
 
-	mw := io.MultiWriter(peers...)
+	mw := io.MultiWriter(...peers)
 	return gob.NewEncoder(mw).Encode(msg)
 
 }
@@ -111,49 +120,45 @@ func (s *FileServer) StoreData(key string, r io.Reader) error {
 	// 1. Store this file to disk
 	// 2. broadcast this file to all known peers
 
-	buf := new(bytes.Buffer)
-	msg := Message{
-		Payload: []byte("storagekey"),
+	var (
+		fileBuffer = new(bytes.Buffer)
+		tee        = io.TeeReader(r, fileBuffer)
+	)
+
+	size, err := s.store.Write(key, r)
+	if err != nil {
+		return err
 	}
-	if err := gob.NewEncoder(buf).Encode(msg); err != nil {
+
+	msg := Message{
+		Payload: MessageStoreFile{
+			Key:  key,
+			Size: size,
+		},
+	}
+
+	msgBuf := new(bytes.Buffer)
+	if err := gob.NewEncoder(msgBuf).Encode(msg); err != nil {
 		return err
 	}
 
 	for _, peer := range s.peers {
-		if err := peer.Send(buf.Bytes()); err != nil {
+		if err := peer.Send(msgBuf.Bytes()); err != nil {
 			return err
 		}
 	}
 
 	time.Sleep(time.Second * 3)
 
-	payload := []bytes("THIS LARGE FILE")
 	for _, peer := range s.peers {
-		if err := peer.Send(payload); err != nil {
+		n, err := io.Copy(peer, fileBuffer)
+		if err != nil {
 			return err
 		}
 
+		fmt.Println("received and returned bytes to disk: ", n)
+	}
 	return nil
-	// if err := s.store.Write(key, r); err != nil {
-	// 	return err
-	// }
-
-	// buf := new(bytes.Buffer)
-	// tee := io.TeeReader(r, buf)
-
-	// if err := s.store.Write(key, tee); err != nil {
-	// 	return err
-	// }
-
-	// p := &DataMessage{
-	// 	Key:  key,
-	// 	Data: buf.Bytes(),
-	// }
-
-	// return s.broadcast(&Message{
-	// 	From:    "todo",
-	// 	Payload: p,
-	// })
 }
 
 func (s *FileServer) Stop() {
@@ -195,4 +200,8 @@ func (s *FileServer) Start() error {
 	s.loop()
 
 	return nil
+}
+
+func init() {
+	gob.Register(MessageStoreFile{})
 }
